0da56b0b2b6730ab4b6f730af9f31208c69b1379
jbedoyav <jbedoyav@grupo-exito.com>
2024-02-09 00:39:32 -0500

    📦 NEW: Add landing-page content-type

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,30 +1,13 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface CarrouselCarrouselApp extends Schema.Component {
-  collectionName: 'components_carrousel_carrousel_app';
+export interface CarouselsCarouselapp extends Schema.Component {
+  collectionName: 'components_carousels_carouselapp';
   info: {
-    displayName: 'Carrousel App';
-    description: 'Carrusel de productos';
+    displayName: 'Carousel App';
+    description: '';
   };
   attributes: {
-    title: Attribute.String;
-    numberMaxOfPluShow: Attribute.Integer;
-    logic: Attribute.Enumeration<
-      [
-        'f%3AALSO_BOUGHT%2Cl%3A5%2Co%3A0',
-        'f%3ASEARCH%2Cl%3A5%2Co%3A0',
-        'f%3ADEPARTMENT%2Cl%3A5%2Co%3A0',
-        'f%3ACATEGORY%2Cl%3A5%2Co%3A0',
-        'f%3ARELATED%2Cl%3A5%2Co%3A0',
-        'f%3ACART%2Cl%3A5%2Co%3A0',
-        'f%3AHOME%2Cl%3A5%2Co%3A0',
-        'f%3APERSONAL%2Cl%3A5%2Co%3A0',
-        'f%3APOPULAR%2Cl%3A5%2Co%3A0',
-        'UsualOrder',
-        ''
-      ]
-    >;
-    complement: Attribute.String;
+    Banners: Attribute.Media;
   };
 }
 
@@ -58,7 +41,7 @@ export interface CategoriesStandarCategories extends Schema.Component {
     displayName: 'Standar Categories';
   };
   attributes: {
-    lista_categorias: Attribute.Enumeration<['standar']>;
+    listOfcategories: Attribute.Enumeration<['standar']>;
   };
 }
 
@@ -69,15 +52,15 @@ export interface LadingPagesStandarLandingPage extends Schema.Component {
     description: '';
   };
   attributes: {
-    Carrousel: Attribute.Component<'carrousel.carrousel-app'>;
-    Categories: Attribute.Component<'categories.categories-pasillos-recomendados'>;
+    carousel: Attribute.Component<'carousels.carouselapp'>;
+    categories: Attribute.Component<'categories.categories-pasillos-recomendados'>;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'carrousel.carrousel-app': CarrouselCarrouselApp;
+      'carousels.carouselapp': CarouselsCarouselapp;
       'categories.categories-pasillos-recomendados': CategoriesCategoriesPasillosRecomendados;
       'categories.standar-categories': CategoriesStandarCategories;
       'lading-pages.standar-landing-page': LadingPagesStandarLandingPage;
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,38 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiLandingPageLandingPage extends Schema.CollectionType {
+  collectionName: 'landing_pages';
+  info: {
+    singularName: 'landing-page';
+    pluralName: 'landing-pages';
+    displayName: 'Landing Page';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    landingPage: Attribute.DynamicZone<['lading-pages.standar-landing-page']> &
+      Attribute.Required;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::landing-page.landing-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::landing-page.landing-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -791,6 +823,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::landing-page.landing-page': ApiLandingPageLandingPage;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
